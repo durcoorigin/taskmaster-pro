fca047bf7d962a485dee3c84491fe1c391fb996c
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-04-15 04:10:19 -0500

    Update script.js
    add edit task feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,10 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+
+// ****** change/update task
 $(".list-group").on("click", "p", function() {
+
   var text = $(this)
     .text()
     .trim();
@@ -56,22 +59,73 @@ $(".list-group").on("click", "p", function() {
   $(this).replaceWith(textInput);
     textInput.trigger("focus");
     
-  $(".list-group").on("blur", "textarea",function() {
-    var text = $(this)
-      .val()
-      .trim();
-
-    var status = $(this)
-      .closest(".list-group")
-      .attr("id")
-      .replace("list-", "");
-
-    var index = $(this)
-    closest(".list-group-item")
-    .index();
-  });
 });
 
+
+$(".list-group").on("blur", "textarea",function() {
+  var text = $(this)
+    .val()
+    .trim();
+
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  $(this).replaceWith(taskP);
+});
+
+
+// ****** change/update date
+$(".list-group").on("click", "span", function() {
+  var date=$(this)
+    .text()
+    .trim();
+
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  $(this).replaceWith(dateInput);
+
+  dateInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "input[type='text']", function() {
+  var date = $(this)
+    .val()
+    .trim();
+
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  tasks[status][index].date = date;
+  saveTasks();
+
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary-badge-pill")
+    .text(date);
+
+  $(this).replaceWith(taskSpan);
+});
 
 
 // modal was triggered
@@ -119,5 +173,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
